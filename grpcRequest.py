e8637d5c0d25c4a81a55a5c5806659eb01dd286a
YONGXI YANG <reganyang0415@gamil.com>
2020-02-28 22:59:22 +0800

    update new version for grpc

--- grpcRequest.py
+++ grpcRequest.py
@@ -1,12 +1,13 @@
 import numpy as np
 
 import tensorflow as tf
-from tensorflow_serving.apis import predict_pb2, prediction_service_pb2
-from grpc.beta import implementations
+from tensorflow_serving.apis import predict_pb2, prediction_service_pb2_grpc
+import grpc
 
 
 host = '0.0.0.0'
 port = 8500
+server = host + f":{port}"
 timeout_req = 30.0
 
 # req_data = np.array([[1., 2.], [1., 3.]])
@@ -18,8 +19,10 @@ adids = [["CoCo"]]
 if __name__ == "__main__":
 
     repeat = 10000
-    channel = implementations.insecure_channel(host, port)
-    stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
+    # channel = implementations.insecure_channel(host, int(port))
+    channel = grpc.insecure_channel(server) 
+    # stub = prediction_service_pb2_grpc.beta_create_PredictionService_stub(channel)
+    stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
 
     request = predict_pb2.PredictRequest()
     request.model_spec.name = 'Toy'
