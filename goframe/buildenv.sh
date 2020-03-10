bec0d434c8c4ac4873c2f68516030270aa68f14a
Ezio <reganyang0415@gmail.com>
2020-03-10 23:45:53 +0800

    build env

--- goframe/buildenv.sh
+++ goframe/buildenv.sh
@@ -1,3 +1,10 @@
+rm -rf src/github.com/golang/
+rm -rf src/go.uber.org/
+rm -rf src/golang.org/
+rm -rf src/google.golang.org/
+rm -rf src/gopath
+rm -rf pkg/*
+
 go get -u golang.org/x/net/context
 go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
 go get -u google.golang.org/grpc
