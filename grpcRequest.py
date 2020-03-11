613b0ca169ab85996528cc0dec8b4a9a535d384c
Ezio <reganyang0415@gmail.com>
2020-03-11 15:23:57 +0800

    add files to test xla

--- grpcRequest.py
+++ grpcRequest.py
@@ -14,30 +14,37 @@ timeout_req = 30.0
 req_data = np.array([[1., 2.], [1., 3.]])
 
 if __name__ == "__main__":
-
+    import timeit
     repeat = 10000
     # channel = implementations.insecure_channel(host, int(port))
     channel = grpc.insecure_channel(server) 
     # stub = prediction_service_pb2_grpc.beta_create_PredictionService_stub(channel)
     stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
 
-    request = predict_pb2.PredictRequest()
-    request.model_spec.name = 'Toy'
-    request.model_spec.signature_name = 'serving_default'
+    sT = 0
+    for _ in range(1000):
+        s = timeit.default_timer()
+        request = predict_pb2.PredictRequest()
+        request.model_spec.name = 'Toy_double_xla'
+        request.model_spec.signature_name = 'serving_default'
 
-    # if you have a structure data. example a dictionary,
-    # you should exhausted every key to change them to protobuf patten
-    # requset.input is the same as the keras model input layer.
-    # so if you define a inputlayer 
-    # for f in sparse_features+varlen_features:
-    #     data_proto = tf.make_tensor_proto(post_100[f], dtype=tf.int64)
-    #     request.inputs[f].CopyFrom(data_proto)
+        # if you have a structure data. example a dictionary,
+        # you should exhausted every key to change them to protobuf patten
+        # requset.input is the same as the keras model input layer.
+        # so if you define a inputlayer 
+        # for f in sparse_features+varlen_features:
+        #     data_proto = tf.make_tensor_proto(post_100[f], dtype=tf.int64)
+        #     request.inputs[f].CopyFrom(data_proto)
 
-    data_proto = tf.make_tensor_proto(req_data, dtype=tf.float32)
+        data_proto = tf.make_tensor_proto(req_data, dtype=tf.float32)
 
-    request.inputs["input_1"].CopyFrom(data_proto)
+        request.inputs["input_1"].CopyFrom(data_proto)
 
-    resp = stub.Predict(request, timeout_req)
-
-    print(resp)
+        resp = stub.Predict(request, timeout_req)
+        e = timeit.default_timer()
+        # print(resp)
+        print(f"the duration is {(e-s) * 1000}ms")
+        sT += (e-s)
+    
+    print(f"the average time for request is {sT}ms")
 
