2fab83bfcd5c8894a73e0f375c42a5097856743d
YONGXI YANG <reganyang0415@gamil.com>
2020-07-14 12:43:06 +0800

    add GO111MODULE

--- go/src/env.sh
+++ go/src/env.sh
@@ -2,4 +2,6 @@
 export GOPATH=$(dirname `pwd`)
 # export GOROOT=$(pwd)/src
 export GOBIN=$GOPATH/bin
+# turn on the GOmodule for searching the pkg in current path.
+export GO111MODULE=on
 export PATH=$PATH:$GOPATH/bin
