a46e60b61a4fa46f9557b0c382790be2a65264c8
YONGXI YANG <reganyang0415@gamil.com>
2020-12-01 11:10:28 +0800

    duplicated

--- cpp/cmake-static-lib/src/module-build/README.md
+++
@@ -1,4 +0,0 @@
-# build module
-
-- using `build` with cmake tools on vscode.
-- `cd build` and `make install`
--- cpp/cmake-static-lib/src/module-build/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /cpp/src/module-build
-# Build directory: /cpp/src/module-build/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
